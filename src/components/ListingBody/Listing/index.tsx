e389541a395ce9668f91c526922317219a049179
Lucas <lucasmunizfk@gmail.com>
2025-02-21 17:53:51 -0300

    dynamic listing

--- src/components/ListingBody/Listing/index.tsx
+++ src/components/ListingBody/Listing/index.tsx
@@ -1,32 +1,21 @@
+import { ProductDTO } from '../../../models/Product';
 import './styles.css';
 
-export default function Listing(){
-    return(
+type Props = {
+    products: ProductDTO[];
+}
+
+export default function Listing({ products }: Props) {
+    return (
         <div className="main-page-container container ds-grid">
-            <div className='product-info'>
-                <p>PC Gamer Pro</p>
-                <h2>R$ 1200.00</h2>
-            </div>
-            <div className='product-info'>
-                <p>PC Gamer Pro</p>
-                <h2>R$ 1200.00</h2>
-            </div>
-            <div className='product-info'>
-                <p>PC Gamer Pro</p>
-                <h2>R$ 1200.00</h2>
-            </div>
-            <div className='product-info'>
-                <p>PC Gamer Pro</p>
-                <h2>R$ 1200.00</h2>
-            </div>
-            <div className='product-info'>
-                <p>PC Gamer Pro</p>
-                <h2>R$ 1200.00</h2>
-            </div>
-            <div className='product-info'>
-                <p>PC Gamer Pro</p>
-                <h2>R$ 1200.00</h2>
-            </div>
+            {
+                products.map(product => (
+                    <div key={product.id} className='product-info'>
+                        <p>{product.name}</p>
+                        <h2>R$ {product.price}</h2>
+                    </div>
+                ))
+            }
         </div>
     );
 }
--- src/components/ListingBody/index.tsx
+++ src/components/ListingBody/index.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import Filter from "./Filter";
 import Listing from "./Listing";
+import { ProductDTO } from "../../models/Product";
+import * as productService from '../../services/product-service.ts';
 
 export default function ListingBody(){
+
+    const [products, setProducts] = useState<ProductDTO[]>([])
+
+    useEffect(() => {
+        setProducts(productService.findByPrice(0,Number.MAX_VALUE))
+    }, [])
+
     return(
         <main>
             <section>
                 <Filter />
-                <Listing />
+                <Listing products={products}/>
             </section>
         </main>
     );
